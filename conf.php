f4c97c4cf2b7e2e3eb215a2530a13ed61a58b8d9
Aldo Tobing <aldo_tobing@hotmail.com>
2023-04-14 13:46:51 +0700

    first commit

---
+++ conf.php
@@ -0,0 +1,10 @@
+<?php 
+    $clientId = "c5dfd6db-dcc7-43db-b8d9-fec66312388b";
+    $clientSecret = "9e257a22657626d43f2fd0961069fe84";
+
+    $oauthCallback = "http://localhost:8000/oauth-callback.php";
+
+    $scope = "item_view item_save customer_save sales_invoice_save sales_invoice_view";
+
+    date_default_timezone_set('asia/jakarta')
+?>
---
+++ ~start-php-server.bat
@@ -0,0 +1,2 @@
+cd /cps/scribble/wwwroot/aol-api-php/complete
+php -S 0.0.0.0:8000 -c php.ini
