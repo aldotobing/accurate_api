b0a38a764a0ee8336321c56f252effec893b8bd2
Aldo Tobing <aldo_tobing@hotmail.com>
2024-07-22 10:35:27 +0700

    bima

--- conf.php
+++ conf.php
@@ -1,8 +1,8 @@
 <?php 
-    $clientId = "-";
-    $clientSecret = "-";
+    $clientId = "ff76499d-6bb5-4d79-9953-fd19e16ddd4c";
+    $clientSecret = "ca4ee0862edb6d9c490985b8d86673e1";
 
-    $oauthCallback = "http://localhost:8888/oauth-callback.php";
+    $oauthCallback = "http://localhost:8000/oauth-callback.php";
 
     $scope = "item_view item_save sales_return_view sales_invoice_save sales_invoice_view";
 
